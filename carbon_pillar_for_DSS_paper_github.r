05d2d4c18cf12e2eb744a01b6e62462baac34c61
Nick Povak <napovak@gmail.com>
2022-11-02 12:48:24 -0500

    Add files via upload

---
+++ carbon_pillar_for_DSS_paper_github.r
@@ -0,0 +1,97 @@
+source("D:/Dropbox/TCSI/Manuscripts/DSS Climate Change/scripts/promote_maps_w_hillshade.r")
+source("D:/Dropbox/TCSI/Scripts/emds/mpat_scores.r")
+
+cp <- function(){
+  
+  tcsi_dir <- paste0("D:/Dropbox/TCSI")
+  main_dir <- paste0(tcsi_dir, "/EMDS/Blueprint")
+  mask_015 <- rast(paste0(tcsi_dir, "/Data/Masks/mask_015m.tif"))
+  mask_landis <- rast(paste0(tcsi_dir, "/Data/Masks/mask_180m_landis.tif"))
+  b <- vect(paste0(tcsi_dir, "/Data/Boundaries/TCSI/tcsi_15m_bounds01.shp"))
+  quad_subdir <- "D:/Dropbox/TCSI/Manuscripts/DSS Climate Change/Results/Carbon_Pillar_Simple"
+  dir.create(quad_subdir)
+  
+  
+  #
+  # <-- CARBON -->
+  #
+  print(paste0("STEP 1. Carbon --> ", date()))
+  
+  cn_e01c <- rast(paste0("D:/Dropbox/TCSI/EMDS/Blueprint/carbon_seq_v4_liveC/carbon_current.tif"))
+  
+  # Change this to only a single metric for simplicity
+  cn_e01f <- rast(paste0("D:/Dropbox/TCSI/EMDS/Blueprint/carbon_seq_v4_liveC/carbon_future.tif"))
+  
+  cn_e01 <- rast(list(cn_e01c, cn_e01f))
+  names(cn_e01) <- c('current', 'future')
+  
+  #
+  # PILLAR: CARBON
+  #
+  cn <- terra::project(x = cn_e01, y = mask_015, method = 'ngb')
+  
+  cn_w <- wrap(cn)
+  save(cn_w, file = paste0(quad_subdir, '/cn.rda'))
+  
+  rm(cn_e01, cn_e01c, cn_e01f, cn_w)
+  gc()
+  
+  #
+  # CARBON
+  #
+  # STEP 2. Calculate Adapt-Protect (Impact) and Quadrant (Strategy) scores
+  #          Sample the 2-D graph area with 1) A/P, 2) Monitor, 3) Protect, 4) Adapt, 5) Transform scoring
+  #          Do this at the 1) Element, 2) Pillar, and 3) Ecosystem levels
+  #
+  print(paste0("CARBON SEQUESTRATION ", date()))
+  cn_scores <- quad_scores_xy(cn, dir_name = quad_subdir)
+  timestamp()
+  
+  
+  # single_map(x = "D:/Dropbox/TCSI/Manuscripts/DSS Climate Change/Results/Carbon_Pillar_Simple/tif/current.tif", f = "D:/Dropbox/TCSI/Manuscripts/DSS Climate Change/Results/Carbon_Pillar_Simple/png/current_hillshade.png", l = T, lp = "bottomleft")
+  # single_map(x = "D:/Dropbox/TCSI/Manuscripts/DSS Climate Change/Results/Carbon_Pillar_Simple/tif/future.tif", f = "D:/Dropbox/TCSI/Manuscripts/DSS Climate Change/Results/Carbon_Pillar_Simple/png/future_hillshade.png", l = F)
+  single_map(x = "D:/Dropbox/TCSI/Manuscripts/DSS Climate Change/Results/Carbon_Pillar_Simple/tif/current.tif", 
+             x2 = "D:/Dropbox/TCSI/Manuscripts/DSS Climate Change/Results/Carbon_Pillar_Simple/tif/future.tif", 
+             f = "D:/Dropbox/TCSI/Manuscripts/DSS Climate Change/Figures/Figure - Current - Future SOE.png", 
+             l = T, 
+             lp = "bottomleft", 
+             label = c("A", "B"))
+  
+  # single_map(x = "D:/Dropbox/TCSI/Manuscripts/DSS Climate Change/Results/Carbon_Pillar_Simple/tif/ap.tif", f = "D:/Dropbox/TCSI/Manuscripts/DSS Climate Change/Results/Carbon_Pillar_Simple/png/ap_hillshade.png", l = F)
+  # single_map(x = "D:/Dropbox/TCSI/Manuscripts/DSS Climate Change/Results/Carbon_Pillar_Simple/tif/ap_rescale.tif", f = "D:/Dropbox/TCSI/Manuscripts/DSS Climate Change/Results/Carbon_Pillar_Simple/png/ap_rescale_hillshade.png", l = F)
+  single_map(x = "D:/Dropbox/TCSI/Manuscripts/DSS Climate Change/Results/Carbon_Pillar_Simple/tif/ap.tif", 
+             x2 = "D:/Dropbox/TCSI/Manuscripts/DSS Climate Change/Results/Carbon_Pillar_Simple/tif/ap_rescale.tif", 
+             f = "D:/Dropbox/TCSI/Manuscripts/DSS Climate Change/Figures/Figure - AdaptProtect.png", 
+             l = T, 
+             lp = "bottomleft", 
+             label = c("A", "B"))
+  
+  # for(i in c("monitor", "protect", "adapt", "transform"))
+  #   single_map(x = "D:/Dropbox/TCSI/Manuscripts/DSS Climate Change/Results/Carbon_Pillar_Simple/tif/adapt.tif", f = "D:/Dropbox/TCSI/Manuscripts/DSS Climate Change/Results/Carbon_Pillar_Simple/png/adapt_hillshade.png", l = F)
+  
+  # Composite strategy score (strong, weak)
+  s <- rast(paste0("D:/Dropbox/TCSI/Manuscripts/DSS Climate Change/Results/Carbon_Pillar_Simple/tif/", c("monitor.tif", "protect.tif", "adapt.tif", "transform.tif")))
+  wm <- which.max(s)
+  m <- max(s)
+  wm1 <- wm * 10
+  # Changed to 0.5
+  w1 <- ifel(m >= 0.5, 1, 0)
+  
+  wms <- wm1 + w1
+  
+  
+  strong_cols <- c("#046C9A", "#F98400", "#3D1778", "#5B1A18")
+  weak_cols <- c("#6C9CCC", "#F2AD00", "#8B7EBB", "#FD6467")
+  cols <- c(weak_cols[1], strong_cols[1], weak_cols[2], strong_cols[2], weak_cols[3], strong_cols[3], weak_cols[4], strong_cols[4])
+  # x11(); plot(wms, col = cols, type = "classes")
+  
+  d <- as.data.frame(expand.grid(c("Weak ", "Strong "), c("monitor", "protect", "adapt", "transform")))
+  dl <- list(); for(i in 1:nrow(d)){dl[[i]] <- paste0(d[i, 1], d[i, 2])}
+  lt <- unlist(dl)
+  single_map(x = wms, 
+             f = "D:/Dropbox/TCSI/Manuscripts/DSS Climate Change/Figures/strategy_score_hillshade.png", 
+             cols = cols, type = "classes", l = T, lp = "bottomleft", lt = lt)
+  
+  
+  
+}
---
+++ promote_maps_w_hillshade.r
@@ -0,0 +1,168 @@
+require(terra)
+require(GEOmap)
+require(rasterVis)
+
+
+single_map <- function(x, x2, f, cols, type, l = T, lp = "topright", lt, label){
+  
+    hill <- rast("D:/Dropbox/TCSI/Data/Topography/hillshade_tcsi_30m.tif")  
+
+    #
+    # Figure 8 - Current and Future SOE scores
+    if(class(x) != "SpatRaster")
+      x <- rast(x)
+    
+    if(!missing(x2)){
+      if(class(x2) != "SpatRaster")
+        x2 <- rast(x2)
+    }
+    
+    # dev.new(width = 9, height = 7, noRStudioGD = TRUE)
+    png(filename = f,
+        width = ifelse(missing(x2), 3.75, 7.5), height = 3.75, units = 'in', res = 300, bg = "white")
+    
+    if(!missing(x2))
+      par(mfrow = c(1, 2), omi = c(0, 0, 0, 0), mai = c(0, 0, 0, 0))
+
+    if(missing(cols)){
+      col_rev <- ifelse(length(grep(pattern = "current|future", x = f, ignore.case = T)) == 1, T, F)
+      these_cols <- hcl.colors(n = 8, palette = "Zissou 1", rev = col_rev)
+      
+      plot(x, breaks = seq(-1, 1, 0.25), col = these_cols, axes = F, legend = F, mar = c(0, 0, 0, 0), colNA = "#FFFFFF33")
+      
+      if(missing(lt))
+        leg.txt <- c("-1.00 to -0.75", "-0.75 to -0.50", "-0.50 to -0.25",
+                     '-0.25 to 0.00', "0.00 to 0.25", "0.25 to 0.50", "0.50 to 0.75",
+                     "0.75 to 1.00")
+      
+    }
+      
+    else{
+      these_cols <- cols
+      
+      if(!missing(lt))
+        leg.txt <- lt
+      
+      if(missing(type))
+        type <- "continuous"
+      
+      plot(x, col = these_cols, axes = F, legend = F, type = type, mar = c(0, 0, 0, 0), colNA = "#FFFFFF33")
+    }
+    
+    
+    plot(hill, col = grey(0:100/100, alpha = 0.2), add = T, legend = F, axes = F, mar = c(0, 0, 0, 0), colNA = "#FFFFFF33")
+
+    if(l)
+      legend(lp, fill = these_cols,
+             legend = leg.txt, cex = 0.6, box.lty = 0)
+    
+    
+    if(!missing(label))
+      legend("topleft", box.lty = 0, legend = label[1], cex = 1.3)
+    
+    
+    #
+    # Plot second plot if you want
+    if(!missing(x2)){
+      
+      if(missing(cols)){
+
+        plot(x2, breaks = seq(-1, 1, 0.25), col = these_cols, axes = F, legend = F, mar = c(0, 0, 0, 0), colNA = "#FFFFFF33")
+
+      }
+      
+      else{
+        these_cols <- cols
+
+        if(missing(type))
+          type <- "continuous"
+        
+        plot(x2, col = these_cols, axes = F, legend = F, type = type, mar = c(0, 0, 0, 0), colNA = "#FFFFFF33")
+      }
+      
+      plot(hill, col = grey(0:100/100, alpha = 0.2), add = T, legend = F, axes = F, mar = c(0, 0, 0, 0), colNA = "#FFFFFF33")
+      
+      if(!missing(label))
+        legend("topleft", box.lty = 0, legend = label[2], cex = 1.3)
+    }
+    
+    dev.off()
+    
+    
+}
+
+
+maps <- function(){
+  
+  # Carbon director
+  main_dir <- "D:/Dropbox/TCSI/EMDS/Blueprint/Quad_Scores_XY_30Dec21/carbon/tif"
+  
+  # Manuscript director
+  out_dir <- "D:/Dropbox/TCSI/Manuscripts/DSS Climate Change"
+
+  hill <- rast("D:/Dropbox/TCSI/Data/Topography/hillshade_tcsi_30m.tif")  
+  
+  # Degrees minutes seconds
+  # lon_degs <- seq(-121.2, -120, 0.2)
+  # lon_degs <- dms(lon_degs)
+  # lon_labs <- sapply(1:length(lon_degs[[1]]), function(i) bquote(.(lon_degs$d[i])*degree ~ .(lon_degs$m[i])*minute ~ W))
+  
+  # axis(1, at = lon_degs, lab = do.call(expression, lon_labs))
+  
+  #
+  # Figure 8 - Current and Future SOE scores
+  curr <- rast(paste0(main_dir, '/current.tif'))
+  fut <- rast(paste0(main_dir, '/future.tif'))
+  
+  # dev.new(width = 9, height = 7, noRStudioGD = TRUE)
+  png(filename = "D:/Dropbox/TCSI/Manuscripts/DSS Climate Change/Figure - Current Future.png",
+      width = 7, height = 5, units = 'in', res = 300, bg = "white")
+  # par(mfrow=c(1,3), omi = c(0, 0, 0, 0), mai = c(0, 0, 0, 0))
+  layout(rbind(c(1, 1, 2, 2),
+               c(1, 1, 2, 2),
+               c(1, 1, 2, 2)))#,
+               # c(1, 1, 2, 2),
+              # c(1, 1, 2, 2),
+               # c(3, 3, 3, 3)))
+  plot(curr, breaks = seq(-1, 1, 0.25), col = hcl.colors(n = 8, palette = "Zissou 1", rev = T), axes = F, legend = F, mar = NA)
+  plot(hill, col = grey(0:100/100, alpha = 0.2), add = T, legend = F, axes = F)
+  legend('topleft', legend = "A", cex = 1.5, box.lty = 0, inset = c(-0.1, 0.18))
+  legend("bottomleft",
+         fill = hcl.colors(n = 8, palette = "Zissou 1", rev = T),
+         legend = c("-1.00 to -0.75", "-0.75 to -0.50", "-0.50 to -0.25",
+                    '-0.25 to 0.00', "0.00 to 0.25", "0.25 to 0.50", "0.50 to 0.75",
+                    "0.75 to 1.00"), horiz = F,
+         cex = 0.8, box.lty = 0)
+  
+  plot(fut, breaks = seq(-1, 1, 0.25), col = hcl.colors(n = 8, palette = "Zissou 1", rev = T), axes = F, legend = F, mar = NA)
+  plot(hill, col = grey(0:100/100, alpha = 0.2), add = T, legend = F, axes = F)
+  legend('topleft', legend = "B", cex = 1.5, box.lty = 0, inset = c(-0.1, 0.18))
+  # par(xpd = NA)
+  # plot(0, xlab = "", ylab = "", xaxt = "n", yaxt = "n", type = "n"); box(col = 'white')
+  # legend("center",
+  #        fill = hcl.colors(n = 8, palette = "Zissou 1", rev = T),
+  #        legend = c("-1.00 to -0.75", "-0.75 to -0.50", "-0.50 to -0.25",
+  #                   '-0.25 to 0.00', "0.00 to 0.25", "0.25 to 0.50", "0.50 to 0.75",
+  #                   "0.75 to 1.00"), horiz = T,
+  #        cex = 0.8, box.lty = 0)
+  dev.off()
+  
+  # rasterVis levelplot
+  soeTheme <- rasterTheme(region = hcl.colors(n = 8, palette = "Zissou 1", rev = T))
+  hsTheme <- modifyList(GrTheme(), list(regions=list(alpha=0.2)))
+  
+  x <- raster::stack(c(raster(curr), raster(fut)))
+  
+  # hillshade levelplot
+  x11(width = 8, height = 6)
+  
+  levelplot(x, par.settings = soeTheme)
+  
+  l1 <- levelplot(x[[1]], par.settings = soeTheme, margin = F, colorkey = F) + levelplot(hill, par.settings = hsTheme, margin = F)
+  l2 <- levelplot(x[[2]], par.settings = soeTheme, margin = F, colorkey = F) + levelplot(hill, par.settings = hsTheme, margin = F)
+  
+  png(filename = "D:/Dropbox/TCSI/Manuscripts/DSS Climate Change/tmp.png",
+      width = 7, height = 5, units = 'in', res = 300, bg = "white")
+  grid.arrange(l1, l2, ncol=2)
+  dev.off()
+}
